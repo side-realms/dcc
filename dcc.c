8570ca0b2f7b43bc5337203f66579eafcef44358
doi <koheidoi@uec.ac.jp>
2023-01-11 07:44:41 +0900

    add node

--- dcc.c
+++ dcc.c
@@ -25,6 +25,23 @@ struct Token{ // Token 型
     char *str; // 文字トークンのときの文字
 };
 
+typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+}NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val
+};
+
 
     // 構造体を変数するときは 
     // struct Token Token;
@@ -97,6 +114,58 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
     cur->next = tok;
     tok->str = str;
     return tok;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary(){
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+Node *mul(){
+    Node *node = primary();
+
+    for(;;){
+        if(consume('*')){
+            node = new_node(ND_MUL, node, primary());
+        }else if(condume('/')){
+            node = new_node(ND_DIV, node, primary());
+        }else{
+            return node;
+        }
+    }
+}
+
+Node *expr(){
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }else if(consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }else{
+            return node;
+        }
+    }
 }
 
 // tokenize
